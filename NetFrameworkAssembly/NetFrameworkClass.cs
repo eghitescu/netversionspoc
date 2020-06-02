a6a5e7a4dd344df4422d40166ddc8f2968761b39
Elena Ghitescu <eghitescu@neocasesoftware.com>
2020-06-02 11:33:38 +0200

    Add project files.

---
+++ NetFrameworkAssembly/NetFrameworkClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFrameworkAssembly
+{
+    public class NetFrameworkClass
+    {
+        public string NetFrameworkSpecificProperty => System.Web.Mail.MailFormat.Html.ToString();
+        public string AnyProperty => "Any property from net framewok";
+    }
+}
---
+++ NetStandardAssembly/NetStandardClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using NetFrameworkAssembly;
+
+namespace NetStandardAssembly
+{
+    public class NetStandardClass
+    {
+        public string PropertyFromNetStandard => "NetStandardClassProperty";
+
+        public string MethodThatCallsSpecificNetFramework()
+        {
+            try
+            {
+                var result = new NetFrameworkClass().NetFrameworkSpecificProperty;
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return $"exception caught {e.Message}";
+            }
+        }
+
+        public string PropertyFromNetFramework => (new NetFrameworkClass()).AnyProperty;
+    }
+}
